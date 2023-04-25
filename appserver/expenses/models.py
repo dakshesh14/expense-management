cb6ae6b156fbca021c072e6f80e52be729a4d2d7
Dakshesh Jain <dakshesh.jain14@gmail.com>
2023-04-25 23:45:49 +0530

    chore: admin panel for expense

--- appserver/expenses/models.py
+++ appserver/expenses/models.py
@@ -32,5 +32,9 @@ class Expenses(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "Expense"
+        verbose_name_plural = "Expenses"
+
     def __str__(self):
         return f"{self.name} - {self.amount} by {self.created_by.username}"
